a069b759cb8a62e14a55f8ed103394fb4db052c5
imamhossain94 <imamagun94@gmail.com>
2021-07-28 09:40:51 +0600

    remodified methods and response objects.

--- notice/notice.py
+++ notice/notice.py
@@ -8,32 +8,39 @@ eventsURL = 'https://www.bubt.edu.bd/Home/all_events'
 notice = Blueprint('notice', __name__)
 
 
-def getAllNE(dType):
+def getAllNE(dType, startLimit, endLimit):
     finalData = {
         'data': list()
     }
-
     try:
         url = noticesURL if dType == 'notice' else eventsURL
         NE_HTML = get(url).text
         NE_HTML = BeautifulSoup(str(NE_HTML), 'html.parser').find_all('table')[0]
         rows = BeautifulSoup(str(NE_HTML), 'html.parser').find_all('tr')[1:]
-        for row in rows:
+        for row in rows[int(startLimit): int(endLimit)]:
             cols = row.find_all('td')
+
+            details = getDetails(cols[0].a['href'].strip())
+
+            print(details)
+
             localData = {
                 'title': cols[0].text.strip(),
                 'published_on': cols[2 if dType == 'notice' else 1].text.strip(),
-                'url': cols[0].a['href'].strip()
+                'url': cols[0].a['href'].strip(),
+                'details': details.get('data'),
             }
             if dType == 'notice':
                 localData['category'] = cols[1].text.strip()
+            else:
+                localData['category'] = "Event"
+
             finalData['data'].append(localData)
         print(len(rows))
         with open('test.html', 'w', encoding='utf-8') as f:
             f.write(str(NE_HTML))
         finalData['status'] = 'success'
     except Exception as e:
-        raise
         finalData = {'status': 'failed', 'reason': str(e)}
 
     return finalData
@@ -43,29 +50,21 @@ def getDetails(url):
     finalData = {
         'data': dict()
     }
-
     try:
         noticeHTML = get(url).text
         noticeHTML = BeautifulSoup(str(noticeHTML), 'html.parser').find('div', {'class': 'devs_history_body'})
-        pubDate = noticeHTML.h3.span.text
         baseURL = 'https://www.bubt.edu.bd'
+
         finalData['data'] = {
-            'title': noticeHTML.h3.text.replace(pubDate, '').strip(),
-            'pubDate': pubDate.split(':')[-1].strip(),
             'description': noticeHTML.find('div', {'class': 'event-details'}).text.strip(),
-            'images': [{'url': baseURL + u['src']} for u in noticeHTML.find_all('img')],
-            'downloads': [{'url': u['href']} for u in noticeHTML.find_all('a')]
+            'images': baseURL + noticeHTML.find_all('img')[0]['src'],
         }
         if len(finalData['data']['images']) == 0:
             finalData['data']['images'].append({'url': ''})
-        if len(finalData['data']['downloads']) == 0:
-            finalData['data']['downloads'].append({'url': ''})
-        with open('test.html', 'w', encoding='utf-8') as f:
-            f.write(str(noticeHTML))
-        finalData['status'] = 'success'
+
     except Exception as e:
-        raise
-        finalData = {'status': 'failed', 'reason': str(e)}
+        print(e)
+        finalData['data'] = {'description': 'none', 'images': ''}
 
     return finalData
 
@@ -75,9 +74,9 @@ def dataBUBT(dataType):
     print(request.args)
     if dataType in ['allNotice', 'noticeDetails', 'allEvent', 'eventDetails', 'getResult']:
         if dataType == 'allNotice':
-            data = getAllNE(dType='notice')
+            data = getAllNE(dType='notice', startLimit=request.args.get('sl'), endLimit=request.args.get('el'))
         elif dataType == 'allEvent':
-            data = getAllNE(dType='event')
+            data = getAllNE(dType='event', startLimit=request.args.get('sl'), endLimit=request.args.get('el'))
         elif dataType == 'noticeDetails' or dataType == 'eventDetails':
             url = request.args.get('url')
             if url is not None:
