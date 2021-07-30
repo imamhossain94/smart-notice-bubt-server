b2f341f6aaf8ed607f802696ad6fdd4aa0ff449e
imamhossain94 <imamagun94@gmail.com>
2021-07-30 12:18:01 +0600

    feat: firebase storage

--- app/main.py
+++ app/main.py
@@ -1,39 +1,50 @@
 import os
 import sys
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
+from fcm.fcm import prepareData
 from notice.notice import notice
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from fcm.fcm import prepareData
-from dotenv import load_dotenv
-load_dotenv()
 
+# Load .env file
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Visit https://stackoverflow.com/questions/21214270/how-to-schedule-a-function-to-run-every-hour-on-flask
 # to know more
 
+# Code for local pc
+# For Server
+sys.path.insert(0, os.getcwd() + '/apis')
 
-# Comment line 49-59 and
-# Uncomment line 5,6 & 17-44 to run this project in local pc
+
+def do_something():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=prepareData, trigger="interval", seconds=5)
+    scheduler.start()
+
+
+class MyFlaskApp(Flask):
+    def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+        if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+            with self.app_context():
+                do_something()
+        super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+
+
+app = MyFlaskApp(__name__)
+app.register_blueprint(notice)
+
+
+@app.route('/')
+def welcome():
+    return '<h1 align="center">Successfully Running</h1>'
+
+# Code fore heroku
 # For Server
 # sys.path.insert(0, os.getcwd() + '/apis')
 #
-#
-# def do_something():
-#     scheduler = BackgroundScheduler()
-#     scheduler.add_job(func=prepareData, trigger="interval", seconds=5)
-#     scheduler.start()
-#
-#
-# class MyFlaskApp(Flask):
-#     def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-#         if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
-#             with self.app_context():
-#                 do_something()
-#         super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
-#
-#
-# app = MyFlaskApp(__name__)
-#
+# app = Flask(__name__)
 #
 # app.register_blueprint(notice)
 #
@@ -41,18 +52,3 @@ load_dotenv()
 # @app.route('/')
 # def welcome():
 #     return '<h1 align="center">Successfully Running</h1>'
-#
-
-# Comment line 5,6 & 17-44 also
-# Uncomment line 49-59 to run this project in heroku
-# For Server
-sys.path.insert(0, os.getcwd() + '/apis')
-
-app = Flask(__name__)
-
-app.register_blueprint(notice)
-
-
-@app.route('/')
-def welcome():
-    return '<h1 align="center">Successfully Running</h1>'
--- fcm/fcm.py
+++ fcm/fcm.py
@@ -1,51 +1,96 @@
 import json
-import time
-import requests
 import os.path
+from io import BytesIO
+import firebase_admin
+import requests
+from PIL import Image
+from firebase_admin import credentials, storage
 from notice.notice import *
 
-noticeFile = 'latestNotice.json'
-eventFile = 'latestEvent.json'
+# Auth google admin sdk
+cred = credentials.Certificate('key.json')
+
+# Initialize firebase app with storage bucket
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'smart-notice-bubt.appspot.com'
+})
+
+# Directory information
+baseDirectory = 'resources'
+noticeFile = baseDirectory + '/latestNotice.json'
+eventFile = baseDirectory + '/latestEvent.json'
+imageFile = baseDirectory + '/photo.jpg'
+
+# Cloud messaging instance
 serverToken = os.environ.get('FCM_SERVER_TOKEN', '')
 deviceToken = os.environ.get('FCM_DEVICE_TOKEN', '')
 topic = os.environ.get('FCM_TOPIC', '')
 
+# Header for fcm
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'key=' + serverToken,
 }
 
 
+# Upload file in firebase storage
+def upload_blob():
+    bucket = storage.bucket()
+    blob = bucket.blob('image.jpg')
+    outfile = imageFile
+    blob.upload_from_filename(outfile)
+    blob.make_public()
+    return blob.public_url
+
+
+# Create push notification payload an send.
 def sendPushNotification(nType, data):
+    # get image url
     imageUrl = data['data'][0]['details']['images']
 
+    # checking if image url is not null
+    if imageUrl:
+        # Download image from url
+        response = requests.get(imageUrl)
+        img = Image.open(BytesIO(response.content))
+        # Resize image url 1000px * 500px to reduce image size under 1MB
+        img = img.resize((1000, 500), Image.ANTIALIAS)
+        # Save image in file
+        img.save(imageFile, 'JPEG')
+        # Upload image into firebase storage and get image url
+        imageUrl = upload_blob()
+
+    # if imageUrl is null then we will send title and body.
     notification = {
         'title': data['data'][0]['title'],
         'body': data['data'][0]['published_on'],
     } if not imageUrl else {
         'title': data['data'][0]['title'],
         'body': data['data'][0]['published_on'],
-        'image': imageUrl
+        'image': imageUrl  # imageUrl
     }
 
     body = {
         'notification': notification,
-        'to': deviceToken, # topic
+        'to': topic,  # topic deviceToken
         'priority': 'high',
         'data': data['data'][0]
     }
-    response = requests.post("https://fcm.googleapis.com/fcm/send", headers=headers, data=json.dumps(body))
 
+    # Sending notification
+    requests.post("https://fcm.googleapis.com/fcm/send", headers=headers, data=json.dumps(body))
+
+    # Saving last send notification data
     with open(noticeFile if nType == 'notice' else eventFile, 'w+') as f:
         f.write(json.dumps(data))
 
-    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
-    print(response.status_code)
-    print(response.json())
-
 
 def localData(objData, filename):
+    dir_exists = os.path.exists(baseDirectory)
     file_exists = os.path.exists(filename)
+
+    if not dir_exists:
+        os.makedirs(baseDirectory)
     if file_exists:
         with open(filename, 'r') as f:
             data = json.loads(f.read())
@@ -55,7 +100,7 @@ def localData(objData, filename):
 
 
 def prepareData():
-    noticeData = getAllNE(dType='notice', page=0, limit=1)
+    noticeData = getAllNE(dType='notice', page=1, limit=1)
     # print("hit push notification")
     # sendPushNotification(nType='notice', data=noticeData)
 
@@ -69,4 +114,3 @@ def prepareData():
         sendPushNotification(nType='event', data=eventData)
     else:
         print("No New Event")
-
